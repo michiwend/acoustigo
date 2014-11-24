a164c627316c16f2300b042a75d042e55368d799
Michael Wendland <accounts@michiwend.com>
2014-11-24 17:20:57 +0100

    adds simple test

---
+++ test/test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/michiwend/acoustigo"
+)
+
+func main() {
+
+	client, _ := acoustigo.NewABClient("http://acousticbrainz.org")
+
+	result, err := client.HighLevel("96685213-a25c-4678-9a13-abd9ec81cf35")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for k, v := range result.HighLevel {
+		fmt.Printf("Found high level data: %s\n", k)
+		fmt.Printf("Value: %s with probability of %f\n\n", v.Value, v.Probability)
+	}
+
+}
